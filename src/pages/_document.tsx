30b7baad4b04d8102a200dbc3a02763f9fa0ef6b
Liam Mahoney <liammahoney96@gmail.com>
2023-09-13 22:16:45 -0500

    fixes x-overflow

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
       <body>
         <Main />
